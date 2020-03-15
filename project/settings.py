74b0249e0ce2e77da9da6f4e0ca7fdb3ee82995c
makotokomizo <poiprince8888@gmail.com>
2020-03-15 15:14:29 +0900

    third commit win

--- project/settings.py
+++ project/settings.py
@@ -183,9 +183,9 @@ DATABASES = {
 db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
 DATABASES['default'].update(db_from_env)
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [
-os.path.join(BASE_DIR, 'static'),
-]
+# STATICFILES_DIRS = [
+# os.path.join(BASE_DIR, 'static'),
+# ]
 SECRET_KEY = os.environ['SECRET_KEY']
 import django_heroku #追加
 django_heroku.settings(locals()) #追加
