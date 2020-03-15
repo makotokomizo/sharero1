b4f14c5cdef1d5b1c28ce9e58838cefe972b19be
makotokomizo <poiprince8888@gmail.com>
2020-03-15 14:35:24 +0900

    third commit win

--- project/settings.py
+++ project/settings.py
@@ -180,7 +180,7 @@ DATABASES = {
 }
 db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
 DATABASES['default'].update(db_from_env)
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
 os.path.join(BASE_DIR, 'static'),
 ]
